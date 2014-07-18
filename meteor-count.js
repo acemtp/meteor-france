f2ff445c56245533193f4efc36efd03deb107fc3
David Bourguignon <contact@davidbourguignon.net>
2014-07-18 21:39:33 +0200

    log httpheaders

--- meteor-count.js
+++ meteor-count.js
@@ -5,7 +5,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     Meteor.methods({
 	  addCount: function (projectID) {
-		console.log('on server, getCurrentTime called');
+        console.log('on server, get time', this.connection.httpHeaders);
 		return new Date();
 	  }
     });
