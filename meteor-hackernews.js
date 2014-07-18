aab0dbc398d4d897634770238c7bdcc8410fdd93
Julien <julien.le.coupanec@gmail.com>
2014-07-18 21:35:20 +0200

    Hacker News Markup #1

---
+++ meteor-hackernews.js
@@ -0,0 +1,20 @@
+// Collections
+
+
+HackerNews = new Meteor.Collection('hacker_news_resource');
+
+
+// Client Code
+
+
+if (Meteor.isClient)  {
+
+}
+
+
+// Server Code
+
+
+if (Meteor.isServer) {
+
+}
