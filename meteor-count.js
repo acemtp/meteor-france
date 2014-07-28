8626dacab21807025bc8e9fb879fe555c761ce15
Adrien Joly <adrien.joly@gmail.com>
2014-07-28 21:39:52 +0200

    removed inserts of "localhost" projects + upserts projects on counter call (instead of update, until the project creation page exists)

--- meteor-count.js
+++ meteor-count.js
@@ -2,15 +2,15 @@ if (Meteor.isServer) {
 
   Projects.remove({url:"localhost:007"});
   Projects.remove({url:"localhost:2999"});
-  Projects.insert({url:"localhost:2999"});
+  //Projects.insert({url:"localhost:2999"});
   Projects.remove({url:"localhost:3000"});
-  Projects.insert({url:"localhost:3000"});
+  //Projects.insert({url:"localhost:3000"});
 
   Meteor.methods({
     addCount: function (clientUrl) {
       var host = clientUrl.split("/")[2];
       console.log('[count]', host);
-      Projects.update({url:host}, {$inc:{visits: 1}});
+      Projects.upsert({url:host}, {$inc:{visits: 1}});
       var project = Projects.findOne({url:host});
       console.log("[count] => proj", project);
       return project;
